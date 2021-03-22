4fb661b06de1debe5893d4fbed14d1b29e23bff2
Ateeq <ateeg1999@gmail.com>
2021-03-22 13:41:33 +0200

    fix some problem for the main file and fonts loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import './screens/categories.dart';
 
@@ -19,10 +16,10 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
+              body1: TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
               ),
-              bodyText2: TextStyle(
+              body2: TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
               ),
               title: TextStyle(
